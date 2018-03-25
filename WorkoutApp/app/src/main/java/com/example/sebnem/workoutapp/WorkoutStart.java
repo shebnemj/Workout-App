a3e93121938158dd6654669e2fc77db48b0f2765
Elif Cudi <32675603+chronosXIV@users.noreply.github.com>
2018-03-25 02:50:16 +0100

    made a countdown before the workout begins!
    although the size is kinda weird? CHARLIE HE LP

--- WorkoutApp/app/src/main/java/com/example/sebnem/workoutapp/WorkoutStart.java
+++ WorkoutApp/app/src/main/java/com/example/sebnem/workoutapp/WorkoutStart.java
@@ -1,7 +1,9 @@
 package com.example.sebnem.workoutapp;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by sebnem on 24/03/2018.
@@ -9,8 +11,33 @@ import android.support.v7.app.AppCompatActivity;
 
 public class WorkoutStart extends AppCompatActivity {
 
+    TextView workoutStartTimer;
+    private CountDownTimer countDownTimer;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workout);
+        setContentView(R.layout.activity_workout_start_countdown);
+
+        workoutStartTimer = (TextView) findViewById(R.id.workoutSCD);
+        workoutStartCountdown();
+
+    }
+
+    private void workoutStartCountdown(){
+
+        workoutStartTimer.setText("4");
+        countDownTimer = new CountDownTimer(4 * 1000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished){
+                workoutStartTimer.setText("" + millisUntilFinished/1000);
+            }
+
+            public void onFinish(){
+                setContentView(R.layout.activity_workout);
+            }
+        };
+
+        countDownTimer.start();
     }
 }
